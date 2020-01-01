102eba6d37eab716e29c4bd5fec62ff1216e1ad7
imran1234567 <sk2read@gmail.com>
2020-01-01 16:16:37 +0530

    Update Previous Code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,12 @@ import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './servers/server.component';
 import { SubServerComponent } from './sub-server/sub-server.component';
-import { RedComponent } from './red/red.component';
-import { YellowComponent } from './yellow/yellow.component';
-import { GreenComponent } from './green/green.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     SubServerComponent,
-    RedComponent,
-    YellowComponent,
-    GreenComponent
   ],
   imports: [
     BrowserModule,
