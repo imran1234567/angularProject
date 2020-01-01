9d8f8ec5789e6caefc78c3e163025f2cefecca03
imran1234567 <sk2read@gmail.com>
2020-01-01 16:56:33 +0530

    Data Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'Imran';
+  ServerName = 'Apollo';
+  ServerPID = 11;
+  ServerStatus = 'Offline';
+  StatusFlag = false;
+  buttonState = true;
+
+  constructor(){
+    setTimeout(() => {
+      this.buttonState = false;
+    }, 2500);
+  }
+
+  toggleServerStatus(){
+    this.StatusFlag = !this.StatusFlag;
+    if(this.StatusFlag === true){
+      this.ServerStatus = 'Online';
+    }
+    else{
+      this.ServerStatus = 'Offline';
+    }
+    return this.ServerStatus;
+  }
 }
