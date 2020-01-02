7f1b76ecfe31022d3daa254ccea9840cfc5fff79
imran1234567 <sk2read@gmail.com>
2020-01-02 15:50:23 +0530

    Second Assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,26 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  ServerName = 'Apollo';
-  ServerPID = 11;
-  ServerStatus = 'Offline';
-  StatusFlag = false;
-  buttonState = true;
-
-  constructor(){
-    setTimeout(() => {
-      this.buttonState = false;
-    }, 2500);
+  FirstName = '';
+  btnState = true;
+  ClearSearch(){
+    this.FirstName = '';
   }
-
-  toggleServerStatus(){
-    this.StatusFlag = !this.StatusFlag;
-    if(this.StatusFlag === true){
-      this.ServerStatus = 'Online';
-    }
-    else{
-      this.ServerStatus = 'Offline';
-    }
-    return this.ServerStatus;
-  }
+  
+  
 }
